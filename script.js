9acd4b080d0a6f7a2c201a8191ce9e5b79a33054
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-01 11:34:22 +0100

    Selected All HTML elemnts in JS & Dynamically displaying information

--- script.js
+++ script.js
@@ -4,119 +4,108 @@ const questions = [
       type: "multiple",
       difficulty: "easy",
       question: "What does CPU stand for?",
+      possible_answers: ["Central Process Unit", "Central Processing Unit", "Computer Personal Unit", "Central Processor Unit"],
       correct_answer: "Central Processing Unit",
-      incorrect_answers: [
-        "Central Process Unit",
-        "Computer Personal Unit",
-        "Central Processor Unit",
-      ],
+      incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"]
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
-      question:
-        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+      possible_answers: ["Final", "Static", "Private", "Public"],
       correct_answer: "Final",
-      incorrect_answers: ["Static", "Private", "Public"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "The logo for Snapchat is a Bell.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question:
-        "Pointers were not used in the original C programming language; they were added later on in C++.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
+      incorrect_answers: ["Static", "Private", "Public"]
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
-      question:
-        "What is the most preferred image format used for logos in the Wikimedia database?",
+      question: "Which Company Was Founded First?",
+      possible_answers: ["Google", "Netflix", "Amazon", "Apple"],
+      correct_answer: "Apple",
+      incorrect_answers: ["Google", "Amazon", "Netflix"]
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Which Programming Language Was Created First?",
+      possible_answers: ["JavaScript", "c", "Python", "PHP"],
+      correct_answer: "C",
+      incorrect_answers: ["JavaScript", "Python", "PHP"]
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What is the most preferred image format used for logos in the Wikimedia database?",
+      possible_answers: [".jpeg", ".png", ".svg", ".gif"],
       correct_answer: ".svg",
-      incorrect_answers: [".png", ".jpeg", ".gif"],
+      incorrect_answers: [".png", ".jpeg", ".gif"]
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
       question: "In web design, what does CSS stand for?",
+      possible_answers: ["Counter Strike: Source", "Cascading Style Sheet", "Corrective Style Sheet", "Computer Style Sheet",],
       correct_answer: "Cascading Style Sheet",
-      incorrect_answers: [
-        "Counter Strike: Source",
-        "Corrective Style Sheet",
-        "Computer Style Sheet",
-      ],
+      incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
-      question:
-        "What is the code name for the mobile operating system Android 7.0?",
+      question: "What is the code name for the mobile operating system Android 7.0?",
+      possible_answers: ["Nougat", "Ice Cream Sandwich", "Jelly Bean", "Marshmallow",], 
       correct_answer: "Nougat",
-      incorrect_answers: [
-        "Ice Cream Sandwich",
-        "Jelly Bean",
-        "Marshmallow",
-      ],
+      incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
       question: "On Twitter, what is the character limit for a Tweet?",
-      correct_answer: "140",
-      incorrect_answers: ["120", "160", "100"],
-    },
-    {
-      category: "Science: Computers",
-      type: "boolean",
-      difficulty: "easy",
-      question: "Linux was first created as an alternative to Windows XP.",
-      correct_answer: "False",
-      incorrect_answers: ["True"],
+      possible_answers: ["140", "180", "240", "280"],
+      correct_answer: "280",
+      incorrect_answers: ["240", "180", "140"],
     },
     {
       category: "Science: Computers",
       type: "multiple",
       difficulty: "easy",
-      question:
-        "Which programming language shares its name with an island in Indonesia?",
+      question: "When Was The First iPhone Released?",
+      possible_answers: ["2006", "2007", "2008", "2009"],
+      correct_answer: "2007",
+      incorrect_answers: ["2006", "2008", "2009"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "Which programming language shares its name with an island in Indonesia?",
+      possible_answers: ["Python", "C", "Jakarta", "Java"], 
       correct_answer: "Java",
       incorrect_answers: ["Python", "C", "Jakarta"],
     },
   ];
 
-//   RULES:
-//         / The player must guess correctly a certain amount of questions
-//         / Each correct answer gives him one point
-//         / Answers could be multiple or true/false
-//         / At the end of the game, the user must know his/her total score
 
-//         QUESTIONS:
-//         / You can get them from this URL ( http://bit.ly/strive_QUIZZ ) or you can write your own
-//         / Could be multiple of boolean (true / false)
+let currentQuestion = 1
+let score = 0
 
-//         HINTS:
-//         / Keep a global variable score for the user score
-//         / Keep a variable questionNumber for the question the user is answering
-//         / When questionNumber is bigger then the available questions, present the score
-//         / Start working with the questions saved in a variable (or you can use AJAX for fetching external questions if you already know how to do it!)
-//         / Start with the easier version and THEN implement the EXTRAs
-//         / Please debug everything / try it on the console to be sure of what to expect from your code
+let questionHeader = document.getElementById('question-header')
+let scoreHeader = document.getElementById('score-header')
+let questionNumber = document.getElementById('question-number')
+let question = document.getElementById('question')
+let answerChoices = document.getElementsByClassName('answer-choice')
+let nextQuestion = document.getElementById('next-question')
 
-//         EXTRA:
-//         / Show if the answer provided was the wrong one or correct it after clicking
-//         / Present the questions one at a time instead of having all of them displayed together
-//         / Let the user
+questionHeader.innerText = `Question - ${currentQuestion}/${questions.length}`
+scoreHeader.innerText = `Score - ${score}`
+questionNumber.innerText = `Question ${currentQuestion}`
+question.innerText = questions[currentQuestion - 1].question
+for (let i = 0; i < questions[currentQuestion - 1].possible_answers.length; i++) {
+  answerChoices[i].innerText = questions[currentQuestion - 1].possible_answers[i]
+}
