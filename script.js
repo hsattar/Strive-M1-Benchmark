dbd28c7c3f25f7728e445cc4256ca75bac9acac2
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-01 15:42:43 +0100

    Fixed Some General Styling

--- script.js
+++ script.js
@@ -17,79 +17,79 @@ const questions = [
       correct_answer: "Final",
       incorrect_answers: ["Static", "Private", "Public"]
     },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "Which Company Was Founded First?",
-      possible_answers: ["Google", "Netflix", "Amazon", "Apple"],
-      correct_answer: "Apple",
-      incorrect_answers: ["Google", "Amazon", "Netflix"]
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "Which Programming Language Was Created First?",
-      possible_answers: ["JavaScript", "C", "Python", "PHP"],
-      correct_answer: "C",
-      incorrect_answers: ["JavaScript", "Python", "PHP"]
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "What is the most preferred image format used for logos in the Wikimedia database?",
-      possible_answers: [".jpeg", ".png", ".svg", ".gif"],
-      correct_answer: ".svg",
-      incorrect_answers: [".png", ".jpeg", ".gif"]
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "In web design, what does CSS stand for?",
-      possible_answers: ["Counter Strike: Source", "Cascading Style Sheet", "Corrective Style Sheet", "Computer Style Sheet",],
-      correct_answer: "Cascading Style Sheet",
-      incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "What is the code name for the mobile operating system Android 7.0?",
-      possible_answers: ["Nougat", "Ice Cream Sandwich", "Jelly Bean", "Marshmallow",], 
-      correct_answer: "Nougat",
-      incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "On Twitter, what is the character limit for a Tweet?",
-      possible_answers: ["140", "180", "240", "280"],
-      correct_answer: "280",
-      incorrect_answers: ["240", "180", "140"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "When Was The First iPhone Released?",
-      possible_answers: ["2006", "2007", "2008", "2009"],
-      correct_answer: "2007",
-      incorrect_answers: ["2006", "2008", "2009"],
-    },
-    {
-      category: "Science: Computers",
-      type: "multiple",
-      difficulty: "easy",
-      question: "Which programming language shares its name with an island in Indonesia?",
-      possible_answers: ["Python", "C", "Jakarta", "Java"], 
-      correct_answer: "Java",
-      incorrect_answers: ["Python", "C", "Jakarta"],
-    },
-  ];
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "Which Company Was Founded First?",
+    //   possible_answers: ["Google", "Netflix", "Amazon", "Apple"],
+    //   correct_answer: "Apple",
+    //   incorrect_answers: ["Google", "Amazon", "Netflix"]
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "Which Programming Language Was Created First?",
+    //   possible_answers: ["JavaScript", "C", "Python", "PHP"],
+    //   correct_answer: "C",
+    //   incorrect_answers: ["JavaScript", "Python", "PHP"]
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    //   possible_answers: [".jpeg", ".png", ".svg", ".gif"],
+    //   correct_answer: ".svg",
+    //   incorrect_answers: [".png", ".jpeg", ".gif"]
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "In web design, what does CSS stand for?",
+    //   possible_answers: ["Counter Strike: Source", "Cascading Style Sheet", "Corrective Style Sheet", "Computer Style Sheet",],
+    //   correct_answer: "Cascading Style Sheet",
+    //   incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "What is the code name for the mobile operating system Android 7.0?",
+    //   possible_answers: ["Nougat", "Ice Cream Sandwich", "Jelly Bean", "Marshmallow",], 
+    //   correct_answer: "Nougat",
+    //   incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "On Twitter, what is the character limit for a Tweet?",
+    //   possible_answers: ["140", "180", "240", "280"],
+    //   correct_answer: "280",
+    //   incorrect_answers: ["240", "180", "140"],
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "When Was The First iPhone Released?",
+    //   possible_answers: ["2006", "2007", "2008", "2009"],
+    //   correct_answer: "2007",
+    //   incorrect_answers: ["2006", "2008", "2009"],
+    // },
+    // {
+    //   category: "Science: Computers",
+    //   type: "multiple",
+    //   difficulty: "easy",
+    //   question: "Which programming language shares its name with an island in Indonesia?",
+    //   possible_answers: ["Python", "C", "Jakarta", "Java"], 
+    //   correct_answer: "Java",
+    //   incorrect_answers: ["Python", "C", "Jakarta"],
+    // }
+  ]
 
 let currentQuestion = 1
 let score = 0
@@ -106,8 +106,8 @@ let answerChoices = document.getElementsByClassName('answer-choice')
 let nextQuestion = document.getElementsByClassName('next-question')[0]
 
 const displayQuiz = () => {
-  welcomeContainer.classList.add('hide-welcome')
-  quizContainer.classList.remove('hide-quiz')
+  welcomeContainer.classList.add('hide-item')
+  quizContainer.classList.remove('hide-item')
 }
 
 startQuiz.addEventListener('click', displayQuiz)
@@ -119,8 +119,8 @@ const checkForLastQuestion = () => {
 }
 
 const updatePpage = () => {
-  questionHeader.innerText = `Question - ${currentQuestion}/${questions.length}`
-  scoreHeader.innerText = `Score - ${score}`
+  questionHeader.innerText = `Question: ${currentQuestion} / ${questions.length}`
+  scoreHeader.innerText = `Score: ${score}`
   questionNumber.innerText = `Question ${currentQuestion}`
   question.innerText = questions[currentQuestion - 1].question
   for (let i = 0; i < questions[currentQuestion - 1].possible_answers.length; i++) {
@@ -132,7 +132,7 @@ const updatePpage = () => {
 updatePpage()
 
 const updateScore = () => {
-  scoreHeader.innerText = `Score - ${score}`  
+  scoreHeader.innerText = `Score: ${score}`  
 }
 
 const checkAnswer = event => {
